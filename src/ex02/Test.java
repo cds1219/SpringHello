a16256a6c39addef1a804951a54e8e659b58f195
cds1219 <cds1219_@naver.com>
2021-06-25 17:48:51 +0900

    좋은 객체 지향 설계 방법(스프링이라서가 아님)

--- src/ex02/Test.java
+++ src/ex02/Test.java
@@ -1,14 +1,14 @@
 package ex02;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class Test {
 	public static void main(String[] args) {
-		Shape c=new Circle();
-		c.area();
+		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("beans.xml"));
 		
-		Shape r=new Rectangle();
-		r.area();
-		
-		Shape t=new Triangle();
-		t.area();
+		Shape s=(Shape) factory.getBean(args[0]);	//arg -> c, r, t 중 하나를 넣어주면 된다
+		s.area();
 	}
 }
