469cfe82f36277484944b78833a43f239c644fa7
cds1219 <cds1219_@naver.com>
2021-06-28 10:38:37 +0900

    스프링 사용을 했다

--- src/ex03/Test.java
+++ src/ex03/Test.java
@@ -1,8 +1,15 @@
 package ex03;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class Test {
 	public static void main(String[] args) {
-		MemberService service=new MemberService();
+		//MemberService service=new MemberService();
+		
+		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("beans.xml"));
+		MemberService service=(MemberService) factory.getBean("memberService");
 		service.selectAll();
 	}
 }
