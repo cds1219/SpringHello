c4816af0fbcd27c0e0bce5a3ba58156492676ce3
cds1219 <cds1219_@naver.com>
2021-06-28 09:41:09 +0900

    주석 추가

--- src/ex02/Test.java
+++ src/ex02/Test.java
@@ -6,7 +6,7 @@ import org.springframework.core.io.FileSystemResource;
 
 public class Test {
 	public static void main(String[] args) {
-		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("beans.xml"));
+		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("beans.xml"));	//객체 주문서
 		
 		Shape s=(Shape) factory.getBean(args[0]);	//arg -> c, r, t 중 하나를 넣어주면 된다
 		s.area();
